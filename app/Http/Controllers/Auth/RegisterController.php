7b655f8029bf113f30c28088896d82dffc77e0a5
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2021-09-04 23:20:54 +0500

    Initial commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response('Congratulation, welcome ' . $user['name'], 201);
+    }
+
+}
